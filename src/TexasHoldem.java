46b6879581b0dac8bede338a9f7cdf3dfb4ac5dc
BillDillWillynd <dwandrewsno1@gmail.com>
2024-03-01 18:13:46 +0000

    Adding the TexasHoldem class

--- src/TexasHoldem.java
+++ src/TexasHoldem.java
@@ -1,31 +1,32 @@
 import java.util.*;
 
 public class TexasHoldem {
-    static int Roundcounter = 0;
+    static int Roundcounter = 1;
+
+    String[][] deck = {{"2h","2d","2c","2s",},
+            {"3h","3d","3c","3s"},
+            {"4h","4d","4c","4s"},
+            {"5h","5d","5c","5s"},
+            {"6h","6d","6c","6s"},
+            {"7h","7d","7c","7s"},
+            {"8h","8d","8c","8s"},
+            {"9h","9d","9c","9s"},
+            {"10h","10d","10c","10s"},
+            {"11h","11d","11c","11s"}, //11 = Jack
+            {"12h","12d","12c","12s"}, //12 = Queen
+            {"13h","13d","13c","13s"}, //13 = King
+            {"14h","14d","14c","14s"}}; //14 = Ace
 
     public TexasHoldem(){
 
         String[] table = {"_", " _", " _", " _", " _"};
 
-        String[][] deck = {{"2h","2d","2c","2s",},
-                {"3h","3d","3c","3s"},
-                {"4h","4d","4c","4s"},
-                {"5h","5d","5c","5s"},
-                {"6h","6d","6c","6s"},
-                {"7h","7d","7c","7s"},
-                {"8h","8d","8c","8s"},
-                {"9h","9d","9c","9s"},
-                {"10h","10d","10c","10s"},
-                {"11h","11d","11c","11s"}, //11 = Jack
-                {"12h","12d","12c","12s"}, //12 = Queen
-                {"13h","13d","13c","13s"}, //13 = King
-                {"14h","14d","14c","14s"}}; //14 = Ace
 
 
         System.out.println("-------------------------------------------------\n" +
                 " h = hearts, d = diamonds, c = clubs, s = spades" +
                 "\n-------------------------------------------------");
-        shuffle(deck);
+        shufflePersonal();
         printTable(table, Roundcounter);
 
         ArrayList<String> globalHand = new ArrayList<>();
@@ -57,6 +58,12 @@ public class TexasHoldem {
     }
     public void bot1(){
 
+        String[] personalHand = new String[2];
+
+        if(Roundcounter == 1){
+            personalHand = shufflePersonal();
+        }
+
     }
     public void bot2(){
 
@@ -64,18 +71,24 @@ public class TexasHoldem {
     public void bot3(){
 
     }
-    public void shuffle(String[][] deck) {
-        int ranCard = 0;
-        int ranSuit = 0;
+    public String[] shufflePersonal() {
+        String[] resultHand = new String[2];
+        int ranCardY = 0;
+        int ranCardX = 0;
 
-        while(true) {
-            ranCard = (int) (Math.random() * 13);
-            ranSuit = (int) (Math.random() * 4);
-            if(deck[ranCard][ranSuit] != "0"){
-                break;
+        for(int i = 0; i<2; i++) {
+            while (true) {
+                ranCardY = (int) (Math.random() * 13);
+                ranCardX = (int) (Math.random() * 4);
+                if (deck[ranCardY][ranCardX] != "0") {
+                    break;
+                }
             }
+            resultHand[i] = deck[ranCardY][ranCardX];
         }
-        deck[ranCard][ranSuit] = "0";
+        deck[ranCardY][ranCardX] = "0";
+
+        return resultHand;
     }
     public static List<List<String>> Bullshit(List<String> globalHand, List<String> comb) {
 
@@ -101,14 +114,14 @@ public class TexasHoldem {
         }
         return result;
     }
-    public static String[][] correctingBullshit(List<List<String>> comb, String[] personalHand){
+    public static String[][] correctingBullshit(List<List<String>> globalHandComb, String[] personalHand){
         String[][] result = new String[10][5];
 
         for(int i = 0; i<10; i++){
             result[i][0] = personalHand[0];
             result[i][1] = personalHand[1];
             for(int j = 0; j<3; j++){
-                result[i][j+2] = comb.get(i).get(j);
+                result[i][j+2] = globalHandComb.get(i).get(j);
             }
         }
         return result;
