92cc94cf92f859af7fdf088c967c52735610c707
BillDillWillynd <dwandrewsno1@gmail.com>
2024-03-01 11:11:08 +0000

    updatng main and miscllaneous

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new TexasHoldem();
+        Miscellaneous.convertToUpper("hello world");
 
     }
 
--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -4,4 +4,18 @@ public class Miscellaneous {
         text = text.replaceAll("[aeiou]", "");
         System.out.println(text);
     }
+    public static void convertToUpper(String text){
+        char[] arr = new char[text.length()];
+
+        for(int i = 0; i<text.length(); i++){
+            arr[i] = text.charAt(i);
+            if(i == 0){
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+            else if(arr[i-1] == ' '){
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+            System.out.print(arr[i]);
+        }
+    }
 }
