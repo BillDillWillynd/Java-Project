eaa9e5efb578a6afcd0bab59138f4a18bb0252ec
BillDillWillynd <dwandrewsno1@gmail.com>
2024-03-07 15:50:22 +0000

    Added the TexasHoldem class and renamed the previouse TexasHoldem class differently

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -18,4 +18,50 @@ public class Miscellaneous {
             System.out.print(arr[i]);
         }
     }
+    public static void swappingVowels(){
+        String text = "Computer";
+        String finalText;
+        char[] arr = text.toCharArray();
+        int a = 0;
+        int b = text.length()-1;
+        int count = 0;
+        int tempA = 0;
+        int tempB = 0;
+        char temp;
+        while(a<b){
+            for(int i = 0; i<text.length(); i++){
+                if(Vowels(arr[i])){
+                    tempA = i;
+                    count++;
+                    break;
+                }
+            }
+            for(int j = text.length()-1; j>0; j--){
+                if(Vowels(arr[j])){
+                    tempB = j;
+                    count++;
+                    break;
+                }
+            }
+            if(count >= 2){
+                temp = arr[tempA];
+                arr[tempA] = arr[tempB];
+                arr[tempB] = temp;
+            }
+            a = a + tempA;
+            b = b - tempB;
+            count = 0;
+        }
+        for(int i = 0; i<text.length(); i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static boolean Vowels(char ch){
+        ch = Character.toUpperCase(ch);
+        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' ||ch == 'U'){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
